679216188cdfdae16960ebd3f801e05a0702515e
Steve Michael - KF0CHE <78129505+kf0che@users.noreply.github.com>
2025-04-20 01:25:21 -0600

    Update branding.sh

--- scripts/branding.sh
+++ scripts/branding.sh
@@ -3,31 +3,21 @@
 # Script to install wallpaper and logo in Linux Mint
 
 # Variables
-WALLPAPER_PATH="/usr/share/backgrounds/MWM-wallpaper.png"
-LOGO_PATH="/usr/share/icons/MWM-logo.png"
+WALLPAPER_PATH="/usr/share/backgrounds/MWM-logo.png"
 USER_HOME=$(eval echo ~${SUDO_USER})
 
 # Function to copy wallpaper
 install_wallpaper() {
     echo "Installing wallpaper..."
-    if [ -f "custom-wallpaper.png" ]; then
-        sudo cp custom_wallpaper.png "$WALLPAPER_PATH"
+    if [ -f "MWM-logo.png" ]; then
+        sudo cp MWM-logo.png "$WALLPAPER_PATH"
+        echo "Installing desktop wallpaper"
+            cp ../logos/MWM-logo.png /usr/share/backgrounds/
+            cp ../logos/MWM-logo.png /usr/share/backgrounds/warty-final-cinnamom.png
         gsettings set org.cinnamon.desktop.background picture-uri "file://$WALLPAPER_PATH"
         echo "Wallpaper installed successfully."
     else
-        echo "Error: custom_wallpaper.png not found in the current directory."
-        exit 1
-    fi
-}
-
-# Function to copy logo
-install_logo() {
-    echo "Installing logo..."
-    if [ -f "custom_logo.png" ]; then
-        sudo cp custom_logo.png "$LOGO_PATH"
-        echo "Logo installed successfully."
-    else
-        echo "Error: custom_logo.png not found in the current directory."
+        echo "Error: MWM-logo.png not found in the current directory."
         exit 1
     fi
 }
@@ -39,6 +29,5 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 install_wallpaper
-install_logo
 
 echo "Branding installation completed."
